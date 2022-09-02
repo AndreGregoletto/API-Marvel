1d85f97b565b87a8d16cc6d6bc09e8f5266e05cc
André Gregoletto <argregoletto@gmail.com>
2022-09-02 18:00:52 -0300

    marvel api

--- app/Http/Controllers/CatchingApiSpaceFlightController.php
+++ app/Http/Controllers/CatchingApiSpaceFlightController.php
@@ -2,15 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Marvel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Nette\Utils\DateTime;
+use Psy\TabCompletion\Matcher\KeywordsMatcher;
 
 class CatchingApiSpaceFlightController extends Controller
 {
     public function apiConsume()
     {
-        $response = Http::get('https://api.spaceflightnewsapi.net');
+        $date       = New DateTime;
+        $timestamp  = $date->getTimestamp();
+        $publicKey  = '9df1de1561b4af5f774d50b2cad697b3';
+        $privateKey = '3e8ba6aeb54e5c97f236c664bc1c60f4c7a01de1';
 
-        dd($response->json());
+        $keys   = $privateKey.$publicKey;
+        $string = $timestamp.$keys;
+        $hash   = hash('md5', $string);
+
+        $response = Http::get('http://gateway.marvel.com/v1/public/stories?ts='.$timestamp.'&apikey='.$publicKey.'&hash='.$hash);
+
+        $marvel = $response->json();
+        $marvel = $marvel['data']['results'];
+
+        foreach ($marvel as $universe) {
+            // dd($universe['type']);
+            $teste = [
+                'title'       => $universe['title'],
+                'description' => $universe['description'],
+                'type'        => $universe['type'],
+            ];
+
+            Marvel::create($teste);
+        }
+
     }
 }
---
+++ database/migrations/2022_09_02_204227_create_marvels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marvels', function (Blueprint $table) {
+            $table->id();
+            $table->char('title')->nullable();
+            $table->char('description')->nullable();
+            $table->char('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marvels');
+    }
+};
